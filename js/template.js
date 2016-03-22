1d7e0200a7f3fd0ec77e72bad1005d4eb523a45a
xoska74 <xoska74@gmail.com>
2016-03-22 22:31:27 +0900

    add 'open calendar' menu on board

--- js/template.js
+++ js/template.js
@@ -129,9 +129,11 @@ var boardButtonCallback = function(t){
       {
         text: 'Open Calendar',
         callback: function (t) {
-          var xhr = new XMLHttpRequest();
-          xhr.open('GET', "https://calendar.sunrise.am/", true);
-          xhr.send();
+//          var xhr = new XMLHttpRequest();
+//          xhr.open('GET', "https://calendar.sunrise.am/", true);
+//          xhr.send();
+          var win = window.open("https://calendar.sunrise.am/", '_blank');
+          win.focus();
 
           t.closePopup();
         }
@@ -139,6 +141,11 @@ var boardButtonCallback = function(t){
     ]
   });
 };
+
+function OpenInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
 
 var cardButtonCallback = function(t, options){
   var items = Object.keys(funcMap).map(function(funcCode){
